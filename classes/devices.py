ede5446404d4f5888314791eb41cbc4ba0c52968
AleksanderVS <106428709+Zynecut@users.noreply.github.com>
2023-02-12 19:31:46 +0100

    Update devices.py

--- classes/devices.py
+++ classes/devices.py
@@ -5,6 +5,16 @@ class Device:
         self.produsent = produsent
         self.produktNavn = produktnavn
         self.serieNummer = serienummer
+"""
+     self.sensor = None
+     self.actuator = None
+    
+     def CreateSensor( x y z )
+          sensor = Sensor(x y z)
+    
+     def CreateActuator( x y z )
+        actuator = Actuator(x y z)
+"""
 
 
 class Actuator(Device):
@@ -14,24 +24,42 @@ class Actuator(Device):
         self.unit = ""
 
         super().__init__(nr, typ, produsent, produktnavn, serienummer)  # sender variablene til parent class Device
-
-
-class Sensor(Device):
-    def __init__(self, nr: int, typ: str, produsent: str, produktnavn: str, serienummer: str, maaltVerdi):
-        self.maaltVerdi = maaltVerdi
-
-        super().__init__(nr, typ, produsent, produktnavn, serienummer)  # sender variablene til parent class Device
-
+"""
     def __repr__(self):
         return f'Recorded Value: {self.maaltVerdi}, Annotation: {}'
 
     def GetReading(self):
         pass
 
-    def ReadSensorValue(self):
+    def ReadSensorValue(self):  # selve verdien
         pass
 
-    def GetUnit(self):
+    def GetUnit(self):  # Her skal vi sjekke type unit sensoren måler (celcius, %, whatever)
+        pass
+"""
+
+
+class Sensor(Device):
+    def __init__(self, nr: int, typ: str, produsent: str, produktnavn: str, serienummer: str):
+      #  self.maaltVerdi = maaltVerdi
+
+        super().__init__(nr, typ, produsent, produktnavn, serienummer)  # sender variablene til parent class Device
+
+
+
+    def __repr__(self):
+        return f'Recorded Value: {self.GetReading()}, Annotation: {self.GetUnit()}'
+
+    def GetReading(self):
+        pass
+
+    def ReadSensorValue(self):  # selve verdien
+        pass
+
+    def GetUnit(self):  # Her skal vi sjekke type unit sensoren måler (celcius, %, whatever)
         pass
 
 # TODO! Her skal du utvikle din egen design av en klassestruktur med enheter og deres funkjsoner!
+
+
+# lag en def som create sensor
